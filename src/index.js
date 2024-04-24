9d522b891f84f59c50b7bab4c4139c1c8ef6d199
demandtech <demandwork247@gmail.com>
2024-04-24 11:08:16 +0100

    fix: cors issue

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
 import blogRoute from "./routes/blog.route.js";
 import logger from "./middlewares/logger.middleware.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:5173"];
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+				callback(null, true); 
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+	})
+);
 app.use(express.json());
 app.use(logger);
-
-app.get("/", (_, res) => {
-	res.send("Welcome to my Alt School Assignment Five!");
-});
+app.set("view engine", "ejs");
+app.set("views", "src/views");
 
 app.use("/api/auth", authRoute);
 app.use("/api/posts", blogRoute);
